fb1ede4e26197fee9e2c44d7c730920c50652db5
DanielBee96 <beezydaniel96@gmail.com>
2026-06-20 07:31:07 +0200

    Create-dzien-1.py

---
+++ Dzien-1.py
@@ -0,0 +1,6 @@
+# Dzień 1 - 20.06.2026
+# Start na SDE. Cel: praca zdalna w IT.
+
+print("Nazywam się Daniel.")
+print("Mam 30 lat i zaczynam od zera.")
+print("Za 24 miesiące będę analitykiem danych.")
