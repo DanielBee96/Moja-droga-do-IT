83feb9e4c499491437623f8171a233d6283999c9
DanielBee96 <beezydaniel96@gmail.com>
2026-06-20 07:50:10 +0200

    Dzien-1.py

--- Dzien-1.py
+++ Dzien-1.py
@@ -4,3 +4,8 @@
 print("Nazywam się Daniel.")
 print("Mam 30 lat i zaczynam od zera.")
 print("Za 24 miesiące będę analitykiem danych.")
+# Sesja 1 - 20.06.2026
+print("Zaczynam naukę Pythona")
+cel = "Data Analyst"
+print("Mój cel to:", cel)
+print("Dzień 1 zrobiony")
